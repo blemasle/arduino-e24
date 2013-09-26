df99330f3256d25ef4e3682f33cbb251d6ccacac
Bertrand Lemasle <blemasle@gmail.com>
2013-09-26 22:03:57 +0000

    basic methods definitions

--- E24LC256.h
+++ E24LC256.h
@@ -8,12 +8,23 @@
 #endif
 #include <Wire.h>
 
+#define E24LC256_ADDR 0x50
 class E24LC256
 {
 private:
+	void burstWrite(unsigned short addr, byte* value, int length);
+	void burstRead(unsigned short addr, byte* value, int length);
 public:
 	E24LC256(byte addr);
 	~E24LC256();
+	
+	byte read();
+	byte read(unsigned short addr);
+	void read(unsigned short page, byte* data);
+
+	void write(byte value);
+	void write(unsigned short addr, byte value);
+	void write(unsigned short page, byte* data);
 };
 
 #endif
