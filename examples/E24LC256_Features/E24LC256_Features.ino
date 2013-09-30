b7d854516f21f7caa0caf7b5da2c97fb85451c97
Bertrand Lemasle <blemasle@gmail.com>
2013-09-30 22:01:25 +0000

    Byte read & write

--- examples/E24LC256_Features/E24LC256_Features.ino
+++ examples/E24LC256_Features/E24LC256_Features.ino
@@ -1,16 +1,49 @@
 #include <E24LC256.h>
 #include <Wire.h>
 
+#define E24LC256_ADDR 0x50
+E24LC256 e24lc = E24LC256(E24LC256_ADDR);
+
 void setup()
 {
-
-  /* add setup code here */
-
+	Wire.begin();
+	Serial.begin(115200);
+	randomSeed(123456789);
 }
 
 void loop()
 {
+	byte rand = (byte)random(0, 256);
+	byte value;
+	unsigned short addr = (unsigned short)random(65535);
 
-  /* add main program code here */
+	Serial.print("Sending ");
+	Serial.print(rand, HEX);
+	Serial.print(" to ");
+	Serial.print(addr, HEX);
+	Serial.println("...");
 
+	e24lc.write(addr, rand);
+
+	Serial.println("Data sent.");
+
+	Serial.print("Reading ");
+	Serial.print(addr, HEX);
+
+	value = e24lc.read(addr);
+	//value = rand;
+
+	Serial.print(" : ");
+	Serial.print(value, HEX);
+	
+	if(value == rand)
+	{
+		Serial.println(", SUCCESS !");
+	}
+	else
+	{
+		Serial.println(", FAILED !");
+	}
+
+	delay(3000);
 }
